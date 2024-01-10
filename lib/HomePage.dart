12e35a37f93e4774994c181d3d35deb5242cb800
Sparsh Gupta <sparsh8559@gmail.com>
2024-01-10 20:34:00 +0530

    hi again

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -10,6 +10,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: (){},
+              child: const Center(
+                child: Text("Calling",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,),
+              ),
+                            )),
+          ElevatedButton(
+              onPressed: (){},
+              child: const Center(
+                child: Text("Bulk SMS",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,),
+                ),
+              )),
+          ElevatedButton(
+              onPressed: (){},
+              child: const Center(
+                child: Text("Bulk WhatsApp Messsage",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,),
+                ),
+              ))
+        ],
+      ),
+    );
   }
 }
